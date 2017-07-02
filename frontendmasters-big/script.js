6acdc2cdfbc85c12ae61d4bdb00ce96c80d8a546
Bill Kremer <bkrems12@gmail.com>
2017-07-02 13:17:03 -0500

    completed f.e.m. problem 1

--- frontendmasters-big/script.js
+++ frontendmasters-big/script.js
@@ -1,44 +1,61 @@
 onload = function () {
 
+  // var one = d3.select("body").append("svg")
 
   var city = 'New York';
-  var width = 800;
-  var height = 300;
+  // var width = 800;
+  // var height = 600;
   var margin = {top: 20, bottom: 20, left: 20, right: 20};
 
-  // var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-  // var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  var w = 0.8* Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  var h = 0.8* Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
   var data = d3.tsv("data.tsv", fix, function(err, data) {
 
     // data.date = new Date(d3.timeParse("%Y%m%d"));
-    console.log(data);
+    // console.log(data);
 
-    xScale = d3.scaleTime()
+    var xScale = d3.scaleTime()
       .domain(d3.extent(data, function(d) {return d.date;}))
-      .range([margin.left, width - margin.right]);
+      .range([margin.left, w - margin.right]);
 
-    yScale = d3.scaleLinear()
+    var yScale = d3.scaleLinear()
       .domain([0, d3.max(data, function(d) {return d[city];})])
-      .range([0, height - margin.bottom - margin.top]);
+      .range([h - margin.bottom, margin.top]);
 
-    var svg = d3.select("svg").selectAll("rect")
+    var heightScale = d3.scaleLinear()
+      .domain([0, d3.max(data, function(d) {return d[city];})])
+      .range([0,h - margin.bottom - margin.top]);
+
+    var svg = d3.select("svg")
+
+      svg.selectAll("rect")
       .data(data)
       .enter().append("rect")
       .attr("width", 2)
-      .attr("height", function (d) {return yScale(d[city]);})
+      .attr("height", function (d) {return heightScale(d[city]);})
       .attr("x", function (d) {return xScale(d.date);})
       .attr("y", function (d) {return yScale(d[city]);})
       .attr("fill", "black");
 
+      var xAxis = d3.axisBottom()
+        .scale(xScale)
+        .tickFormat(d3.timeFormat('%b %Y'));
 
+      var yAxis = d3.axisLeft()
+        .scale(yScale);
 
+      svg.append("g")
+        .attr("transform", "translate(0," + (h - margin.bottom )+ ")")
+        .call(xAxis)
+;
 
+      svg.append("g")
+        .call(yAxis)
+        .attr("transform", "translate(" +margin.left+",0)");
 
 
   })
-
-
 
   function fix(d) {
     d.date = new Date(d3.timeParse("%Y%m%d")(d.date));
