5201fc2282ac00464669ba351fe815a55e55b093
Bill Kremer <bkrems12@gmail.com>
2017-07-01 22:46:03 -0500

    moved the graph to be able to see it.

--- frontendmasters-big/script.js
+++ frontendmasters-big/script.js
@@ -2,12 +2,12 @@ onload = function () {
 
 
   var city = 'New York';
-  // var width = 800;
-  // var height = 300;
+  var width = 800;
+  var height = 300;
   var margin = {top: 20, bottom: 20, left: 20, right: 20};
 
-  var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-  var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  // var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  // var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
   var data = d3.tsv("data.tsv", fix, function(err, data) {
 
@@ -16,11 +16,11 @@ onload = function () {
 
     xScale = d3.scaleTime()
       .domain(d3.extent(data, function(d) {return d.date;}))
-      .range([margin.left, w - margin.right]);
+      .range([margin.left, width - margin.right]);
 
     yScale = d3.scaleLinear()
       .domain([0, d3.max(data, function(d) {return d[city];})])
-      .range([0, h - margin.bottom - margin.top]);
+      .range([0, height - margin.bottom - margin.top]);
 
     var svg = d3.select("svg").selectAll("rect")
       .data(data)
