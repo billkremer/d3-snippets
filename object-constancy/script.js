6862deb4e4bd8e95d3faf5dc30e545be66c0e322
Bill Kremer <bkrems12@gmail.com>
2017-07-05 14:29:22 -0500

    working on object constancy turtorial.  having trouble updating from d3.v2 to d3.v4

--- object-constancy/script.js
+++ object-constancy/script.js
@@ -10,13 +10,15 @@ onload = function () {
 
 
   var x = d3.scaleLinear()
-    .range([0,width])
+    .range([0,width]);
 
   var y = d3.scaleOrdinal()
-    .range([0,height],0.1)
+    .range([0,height],0.1);
 
     var xAxis = d3.axisTop()
       .scale(x)
+      .tickSize(-height - margin.bottom)
+      .tickFormat(format);
 
   // var yAxis = d3.axisLeft()
   //   .scale(y)
@@ -26,7 +28,7 @@ onload = function () {
     .attr("height", height + margin.top + margin.bottom)
     .style("margin-left", -margin.left +"px")
   .append("g")
-    .attr("transform", "translate("+margin.left+","+margin.top +")")
+    .attr("transform", "translate("+(margin.left+ 20) + ","+margin.top +")")
 
 
   svg.append("g")
@@ -38,7 +40,7 @@ onload = function () {
     .attr("class", "domain")
     .attr("y2",height)
 
-    var menu = d3.select("#menu select")
+    var menu = d3.select("#menu").select("select")
       .on("change",change)
 
     d3.csv("states-age.csv", function (data) {
@@ -54,7 +56,7 @@ onload = function () {
           });
       });
 
-      menu.select("option")
+      menu.selectAll("option") // have to use selectAll, cant use select
         .data(ages)
         .enter().append("option")
         .text(function (d) {return d;});
@@ -67,18 +69,107 @@ onload = function () {
 
     }); // end of csv call
 
+    // hold down alt key to slow down animation to a crawl
+    var altKey;
 
+    d3.select(window)
+      .on("keydown", function () { altKey = d3.event.altKey; })
+      .on("keyup", function() { altKey = "false"; });
 
+function change() {
 
+    clearTimeout(timeout); // clears automatic cycling through list
 
+    d3.transition()
+      .duration(altKey ? 7500 : 750)
+      .each(redraw);
 
-
-
-
-function type(d) {
-  d.value = parseFloat(d.frequency); // coerce to number
-  return d;
 }
+
+function redraw() {
+  var age1 = menu.property("value")
+  var top = states.sort(function(a,b) {return b[age1] - a[age1];}).slice(0,10)
+  // top 10 responses
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+
+  y.domain(top.map(function (d) { return d.State;}   ));
+// console.log(y.domain()); // array of the top ten states "ND", "DC", etc
+
+  var yBand = d3.scaleBand()
+    .domain(top.map(function (d) { return d.State;}   ))
+    .range([0,height],0.1);
+
+      // console.log(yBand.bandwidth()); // 0.1 ?
+
+
+  var bar = svg.selectAll(".bar")
+    .data(top, function (d) {return d.State;});
+
+  var barEnter = bar.enter().insert("g", ".axis") // y axis
+    .attr("class", "bar")
+    .attr("transform", function (d) {
+      console.log( "y", y(d.State) );
+       return "translate(0," + (y(d.State) + height) +  ")"})
+    .style("fill-opacity",0.25);
+
+    barEnter.append("rect")
+      .attr("width", age && function(d) {return x(d[age]); })
+      .attr("height",  yBand.bandwidth());
+
+
+
+    barEnter.append("text") // y axis label, not really axis labels though
+      .attr("class", "label") // just appended to end of bar
+      .attr("x", -3)
+      .attr("y", yBand.bandwidth() /2)
+      .attr("dy", ".35em")
+      .attr("text-anchor", "end")
+      .text(function (d) {return d.State; });
+
+    barEnter.append("text") // bar value placeholder?
+      .attr("class", "value")
+      .attr("x", age && function(d) {return x(d[age])-3;})
+      .attr("y",  yBand.bandwidth()/2)
+      .attr("dy", ".35em")
+      .attr("text-anchor","end")
+      // .text(function (d) {return d[age];})
+
+    x.domain([0, top[0][age = age1]]); // resets the domain.
+
+    var barUpdate = bar.transition()
+      .attr("transform", function (d) {
+        console.log(y(0),"dstate");
+        return "translate(0," + ( y(0)-y(d.State)) + ")";
+      })
+      .style("fill-opacity", 1);
+
+    barUpdate.select("rect")
+      .attr("width", function (d) { return x(d[age]);});
+
+    barUpdate.select(".value")
+      .attr("x", function (d) {return x(d[age])-3})
+      .text(function (d) {return format(d[age]);});
+
+
+    var barExit = bar.transition()
+      .attr("transform", function(d) {return "translate(0, " + ( y(0)+ height) + ")";})
+      .style("fill-opacity", 0.7)
+      .remove();
+
+    barExit.select(".value")
+      .attr("x", function (d) { return x(d[age])-3; })
+      .text(function (d) { return format(d[age])});
+
+    d3.transition(svg).select(".x.axis")
+      .call(xAxis);
+
+}
+
+var timeout = setTimeout(function() {
+  menu.property("value", "65 Years and Over").node().focus();
+  change();
+}, 5000)
+
 
 
 }
